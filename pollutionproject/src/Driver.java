15b06f95965d325b48d18b924b23d578cbed19dc
seyeong <tpdud1116@daum.net>
2019-11-25 03:46:30 +0900

    왜 ignore안되는데

---
+++ pollutionproject/src/Driver.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Driver extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main m1 = new Main();
+		m1.setVisible(true);
+	}
+
+}
+
---
+++ pollutionproject/src/Home.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*; 
+
+public class Home extends JPanel{
+	
+	private JPanel main;
+	private JLabel welcome; // 웰컴!!
+	private JLabel stuNum; // 학번
+
+	public Home() {
+
+		setLayout(new BorderLayout());
+		setSize(1000,600);
+	
+		main = new JPanel();
+		main.setLayout(new BorderLayout());
+
+		welcome = new JLabel("🌸Welcome🌸");
+		main.setBackground(Color.white);
+		welcome.setFont(welcome.getFont().deriveFont(70.0f)); //폰트 크기 변경
+		welcome.setBackground(Color.white);
+		welcome.setHorizontalAlignment(JLabel.CENTER);
+      
+		stuNum = new JLabel("<html>60182208 장세영<br>60182155 김재원<br>60182153 김은주 <br> 60182174 서예지<br><br><br></html>");
+		stuNum.setFont(welcome.getFont().deriveFont(20.0f));
+		stuNum.setBackground(Color.white);
+		stuNum.setHorizontalAlignment(JLabel.CENTER);
+
+		main.add(welcome,BorderLayout.CENTER);
+		main.add(stuNum,BorderLayout.SOUTH);
+	
+		add(main);
+	}
+}
